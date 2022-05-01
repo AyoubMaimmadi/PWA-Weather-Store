a7da9925d5be1810e70b8a22c3e6eb3056f5f671
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-01 12:13:37 +0000

    new

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import { fetchWeather } from './api/fetchWeather'
 import './App.css'
 
 const App = () => {
-  return <h1>App</h1>
+  const [query, setQuery] = React.useState('')
+
+  const search = async (e) => {
+    if (e.key === 'Enter') {
+      const data = await fetchWeather(query)
+    }
+  }
+
+  return (
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </div>
+  )
 }
 
 export default App
