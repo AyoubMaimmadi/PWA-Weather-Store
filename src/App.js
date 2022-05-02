74c4023e5d3ed5e9fe218f5bbaf4e2001bc5322c
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-02 19:51:04 +0000

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { fetchWeather } from './api/fetchWeather'
 import './App.css'
 
 const App = () => {
-  const [query, setQuery] = React.useState('')
-  const [weather, setWeather] = React.useState({})
+  const [query, setQuery] = useState('')
+  const [weather, setWeather] = useState({})
 
   const search = async (e) => {
     if (e.key === 'Enter') {
